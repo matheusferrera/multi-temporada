d36f3fa518190801398cece041a6c1ee92560491
matheus ferreira <mat.matheusf@hotmail.com>
2024-01-19 13:40:07 -0300

    second commit

--- src/Service.js
+++ src/Service.js
@@ -1,29 +1,18 @@
+const axios = require('axios');
+
 async function getAllClients() {
-    const url = 'https://ssmt.stays.com.br/external/v1/booking/clients';
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Basic NzgyODc2N2Q6NzVjNDg0N2I='
-    };
-  
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic NzgyODc2N2Q6NzVjNDg0N2I='
-          },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Erro de rede - ${response.status}`);
-      }
-  
-      const responseData = await response.json();
-      console.log('Resposta get all clients:', responseData);
-    } catch (error) {
-      console.error('Erro:', error);
-    }
+  const url = 'https://ssmt.stays.com.br/external/v1/booking/clients';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic NzgyODc2N2Q6NzVjNDg0N2I='
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    console.log('Resposta get all clients:', response.data);
+  } catch (error) {
+    console.error('Erro:', error.message);
   }
-  
-  getAllClients();
-  
+}
+
+getAllClients();
