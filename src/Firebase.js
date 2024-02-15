2ca216572c3e1687f23b7fa40d5a7656b5271b0b
matheus ferreira <mat.matheusf@hotmail.com>
2024-02-15 11:10:21 -0300

    atualizacao de DB

--- src/Firebase.js
+++ src/Firebase.js
@@ -6,165 +6,203 @@ const {
   child,
   get
 } = require("firebase/database");
-const json = require("./users.json")
+
+const { getFirestore } = require("firebase/firestore")
+const { collection, getDocs, doc, setDoc, getDoc, updateDoc } = require("firebase/firestore");
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBEaOUZRRdJgPT7T7gTjKYecBnDHubcCY4",
+//     authDomain: "multitemporada-backend.firebaseapp.com",
+//     projectId: "multitemporada-backend",
+//     storageBucket: "multitemporada-backend.appspot.com",
+//     messagingSenderId: "697309256251",
+//     appId: "1:697309256251:web:d2cdb2294de3c90a34da1e"
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBEaOUZRRdJgPT7T7gTjKYecBnDHubcCY4",
-    authDomain: "multitemporada-backend.firebaseapp.com",
-    projectId: "multitemporada-backend",
-    storageBucket: "multitemporada-backend.appspot.com",
-    messagingSenderId: "697309256251",
-    appId: "1:697309256251:web:d2cdb2294de3c90a34da1e"
-  };
+  apiKey: "AIzaSyDF9AyJH_TIweKlyRG8uMf8dS3_QKiCAxU",
+  authDomain: "multitemporada-api.firebaseapp.com",
+  databaseURL: "https://multitemporada-api-default-rtdb.firebaseio.com",
+  projectId: "multitemporada-api",
+  storageBucket: "multitemporada-api.appspot.com",
+  messagingSenderId: "17067214021",
+  appId: "1:17067214021:web:6bf8a328f9c834bd3f5cac",
+  measurementId: "G-6DXKVNK2KJ"
+};
 
 // Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
+const db2 = getFirestore(app);
 
-// const createDB = async function () {
-//     let dataRef = ref(db, "Imoveis");
-//     for (const item of json.users) {
-//         const whatsappId = String(item.IDUNIDADE);
-//         try {
-//             await update(dataRef, { [whatsappId]: item }); 
-//             console.error("======> [DB/createDB] INSERIU: " + whatsappId);
-//         } catch (errorObject) {
-//             console.error("======> [DB/createDB] ERRO de acesso: " + errorObject);
-//             return errorObject;
-//         }
+
+// async function getImovel(id) {
+
+//     // Referência ao nó específico no Realtime Database
+//     const dataRef = ref(db, "Imoveis/" + id);
+
+//     try {
+//       // Utiliza await para esperar a conclusão da operação get
+//       const snapshot = await get(dataRef);
+
+//       if (snapshot.exists()) {
+//         // Se o documento existir, snapshot.val() conterá os dados
+//         const data = snapshot.val();
+//         console.log("Dados encontrados para ID", id, ":", data);
+//         return data
+
+//       } else {
+//         console.log("Nenhum dado encontrado para ID", id);
+//       }
+//     } catch (error) {
+//       console.error("Erro ao obter dados para ID", id, ":", error);
 //     }
-//     console.error("<===== TODOS OS DADOS INSERIDOS =====>");
+//   }
+
+
+async function getMessage(id) {
+
+  // Referência ao nó específico no Realtime Database
+  const dataRef = ref(db, "Mensagens/" + id);
+
+  try {
+    // Utiliza await para esperar a conclusão da operação get
+    const snapshot = await get(dataRef);
+
+    if (snapshot.exists()) {
+      // Se o documento existir, snapshot.val() conterá os dados
+      const data = snapshot.val();
+      console.log("Dados encontrados para ID", id, ":", data);
+      return data
+
+    } else {
+      console.log("Nenhum dado encontrado para ID", id);
+    }
+  } catch (error) {
+    console.error("Erro ao obter dados para ID", id, ":", error);
+  }
+}
+
+
+// async function updateImovel(id, newData) {
+//   // Inicializar o Firebase
+
+
+//   // Referência ao nó específico no Realtime Database
+//   const dataRef = ref(db, "Imoveis/" + id);
+
+//   try {
+//     // Utiliza await para esperar a conclusão da operação update
+//     const getRef = await get(dataRef)
+//     const newObjt = {
+//       ...getRef.val(),
+//       ...newData
+//     }
+//     console.log("NEWOBJT ->", newObjt)
+//     await update(dataRef, newObjt);
+
+//     console.log("Dados para ID", id, "atualizados com sucesso:", newData);
 //     return 0
-// };
+//   } catch (error) {
+//     console.error("Erro ao atualizar dados para ID", id, ":", error);
+//   }
+// }
+
+async function updateMessages(id, newData) {
+  // Inicializar o Firebase
+
+
+  // Referência ao nó específico no Realtime Database
+  const dataRef = ref(db, "Mensagens/" + id);
+
+  try {
+    // Utiliza await para esperar a conclusão da operação update
+    const getRef = await get(dataRef)
+    const newObjt = {
+      ...getRef.val(),
+      ...newData
+    }
+    console.log("NEWOBJT ->", newObjt)
+    await update(dataRef, newObjt);
+
+    console.log("Dados para ID", id, "atualizados com sucesso:", newData);
+    return 0
+  } catch (error) {
+    console.error("Erro ao atualizar dados para ID", id, ":", error);
+  }
+}
+
 
 
 async function getImovel(id) {
-  
-    // Referência ao nó específico no Realtime Database
-    const dataRef = ref(db, "Imoveis/" + id);
-  
-    try {
-      // Utiliza await para esperar a conclusão da operação get
-      const snapshot = await get(dataRef);
-  
-      if (snapshot.exists()) {
-        // Se o documento existir, snapshot.val() conterá os dados
-        const data = snapshot.val();
-        return data
-        console.log("Dados encontrados para ID", id, ":", data);
-      } else {
-        console.log("Nenhum dado encontrado para ID", id);
-      }
-    } catch (error) {
-      console.error("Erro ao obter dados para ID", id, ":", error);
+  try {
+
+    if (id == "") {
+      let objResp = {}
+      const querySnapshot = await getDocs(collection(db2, "Imoveis", id));
+      querySnapshot.forEach((doc) => {
+        objResp[doc.id] = doc.data()
+      });
+      return objResp
     }
+
+    const docRef = doc(db2, "Imoveis", id);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data()
+
+  } catch (e) {
+    console.log("[FIREBASE/GETIMOVEL]-> ", e)
   }
+}
 
+async function getMessage2(id) {
+  try {
+    let objResp = {}
 
-  async function getMessage(id) {
-  
-    // Referência ao nó específico no Realtime Database
-    const dataRef = ref(db, "Mensagens/" + id);
-  
-    try {
-      // Utiliza await para esperar a conclusão da operação get
-      const snapshot = await get(dataRef);
-  
-      if (snapshot.exists()) {
-        // Se o documento existir, snapshot.val() conterá os dados
-        const data = snapshot.val();
-        console.log("Dados encontrados para ID", id, ":", data);
-        return data
-        
-      } else {
-        console.log("Nenhum dado encontrado para ID", id);
-      }
-    } catch (error) {
-      console.error("Erro ao obter dados para ID", id, ":", error);
+    const querySnapshot = await getDocs(collection(db2, "Mensagens", id));
+    querySnapshot.forEach((doc) => {
+      objResp[doc.id] = doc.data()
+
+    });
+    console.log("OBJ  --> ", objResp);
+    return objResp
+  } catch (e) {
+    console.log("ERRO -> ", e)
+  }
+}
+
+async function updateImovel(id, newData) {
+  try {
+    const docRef = doc(db2, "Imoveis", id);
+
+    // Verifica se o documento existe
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      // Atualiza o parâmetro específico no documento
+      await updateDoc(docRef, newData);
+
+    } else {
+      console.log(`Documento com ID '${id}' não encontrado.`);
     }
+  } catch (e) {
+    console.error("Erro ao atualizar parâmetro:", e);
   }
+}
 
-  async function getReserva(id) {
-  
-    // Referência ao nó específico no Realtime Database
-    const dataRef = ref(db, "Reservas/" + id);
-  
-    try {
-      // Utiliza await para esperar a conclusão da operação get
-      const snapshot = await get(dataRef);
-  
-      if (snapshot.exists()) {
-        // Se o documento existir, snapshot.val() conterá os dados
-        const data = snapshot.val();
-        console.log("Dados encontrados para ID", id, ":", data);
-        return data
-        
-      } else {
-        console.log("Nenhum dado encontrado para ID", id);
-      }
-    } catch (error) {
-      console.error("Erro ao obter dados para ID", id, ":", error);
-    }
-  }
-  
-  async function updateImovel(id, newData) {
-    // Inicializar o Firebase
 
-  
-    // Referência ao nó específico no Realtime Database
-    const dataRef = ref(db, "Imoveis/" + id);
-  
-    try {
-      // Utiliza await para esperar a conclusão da operação update
-      const getRef = await get(dataRef)
-      const newObjt = {
-        ...getRef.val(),
-        ...newData
-      }
-      console.log("NEWOBJT ->", newObjt)
-      await update(dataRef, newObjt);
-  
-      console.log("Dados para ID", id, "atualizados com sucesso:", newData);
-      return 0
-    } catch (error) {
-      console.error("Erro ao atualizar dados para ID", id, ":", error);
-    }
-  }
+getMessage2("")
 
-  async function updateMessages(id, newData) {
-    // Inicializar o Firebase
 
-  
-    // Referência ao nó específico no Realtime Database
-    const dataRef = ref(db, "Mensagens/" + id);
-  
-    try {
-      // Utiliza await para esperar a conclusão da operação update
-      const getRef = await get(dataRef)
-      const newObjt = {
-        ...getRef.val(),
-        ...newData
-      }
-      console.log("NEWOBJT ->", newObjt)
-      await update(dataRef, newObjt);
-  
-      console.log("Dados para ID", id, "atualizados com sucesso:", newData);
-      return 0
-    } catch (error) {
-      console.error("Erro ao atualizar dados para ID", id, ":", error);
-    }
-  }
-  
-  
+
 const firebase = {
-    getImovel,
-    updateImovel,
-    getReserva,
-    getMessage,
-    updateMessages
+  getImovel,
+  updateImovel,
+  getMessage,
+  updateMessages
 };
 
 module.exports = firebase;
